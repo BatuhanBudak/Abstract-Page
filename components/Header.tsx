e251b1d9d3ad82f4939c25d9bd196df238377818
Batuhan <batuhan.budak@gmail.com>
2022-08-03 23:33:51 +0300

    Start creating Header component

---
+++ components/Header.tsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+import Nav from "./Nav";
+
+import Image from 'next/image';
+import logo from '../public/logo.svg'
+import search from '../public/search.svg';
+
+export default function Header() {
+    const [navToggled, setNavToggled] = useState(false);
+    const [searchToggled, setSearchToggled] = useState(false);
+    const [windowWidth, setWindowWidth] = useState(0);
+
+    function closeNav() {
+        if (windowWidth > 900) return;
+        setNavToggled(false);
+    }
+    function toggleNav() {
+        setNavToggled((prev) => !prev);
+    }
+    function toggleSearch() {
+        setSearchToggled((prev) => !prev);
+    }
+
+
+    function resizeListener() {
+        setWindowWidth(window.innerWidth);
+    }
+
+    useEffect(() => {
+        window.addEventListener("resize", resizeListener);
+
+        return () => window.removeEventListener("resize", resizeListener);
+    });
+
+    return (
+        <header className={navToggled && windowWidth < 900 ? "nav-open" : ""}>
+            <div className="header-left-links">
+                <a className='header-left-link logo-svg' href=""><Image src={logo} alt="Abstract Logo" /></a>
+                <a className='header-left-link header-left-link--second border-left' href=""> Help Center</a>
+            </div>
+            <div className="header-button-group">
+                <button
+                    className="search-toggle"
+                    aria-expanded={searchToggled}
+                    onClick={toggleSearch}
+                    aria-controls="nav-search">
+                    <Image src={search} alt="Abstract Logo" />
+                </button>
+                <button
+                    className="nav-toggle"
+                    aria-expanded={navToggled}
+                    onClick={toggleNav}
+                    aria-controls="primary-navigation"
+                >
+                    <span className="hamburger"></span>
+                    {/* <span className="sr-only">Menu</span> */}
+                </button>
+            </div>
+            <Nav closeNav={closeNav} />
+        </header>
+    );
+}
---
+++ components/Nav.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default function Nav({ closeNav }) {
+    return (
+        <nav className="nav" >
+            <a href="/#services" className="nav__link" onClick={() => closeNav()}>
+                Submit a request
+            </a>
+            <a href="/#services" className="nav__link" onClick={() => closeNav()}>
+                Sign in
+            </a>
+            {/* <ul className="nav__list" id="primary-navigation">
+                <li className="nav__item">
+                    <a href="/#home" className="nav__link" onClick={() => closeNav()}>
+                        Home
+                    </a>
+                </li>
+                <li className="nav__item">
+                    <a href="/#services" className="nav__link" onClick={() => closeNav()}>
+                        Services
+                    </a>
+                </li>
+                <li className="nav__item">
+                    <a href="/#about" className="nav__link" onClick={() => closeNav()}>
+                        About
+                    </a>
+                </li>
+                <li className="nav__item">
+                    <a href="/#work" className="nav__link" onClick={() => closeNav()}>
+                        My Work
+                    </a>
+                </li>
+            </ul> */}
+        </nav>
+    );
+}
